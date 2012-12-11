fd5efae6c2be48fa996e7af035e44fb2acbe90f6
ivar <ivar@flurdy.com>
2012-12-11 00:26:46 +0000

    teno

--- project/Build.scala
+++ project/Build.scala
@@ -12,11 +12,14 @@ object ApplicationBuild extends Build {
       "org.mindrot" % "jbcrypt" % "0.3m",
       "com.typesafe" %% "play-plugins-mailer" % "2.0.4",
       "com.andersen-gott" %% "scravatar" % "1.0.1",
-      "org.mongodb" %% "casbah" % "2.4.1"
+      "org.mongodb" %% "casbah" % "2.4.1",
+      "se.radley" %% "play-plugins-salat" % "1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId"
     )
 
 }
