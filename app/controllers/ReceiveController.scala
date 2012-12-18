b59db3874952c659af639933f1c07316dff607a8
Ivar Abrahamsen <ivar@flurdy.com>
2012-12-18 20:47:59 +0000

    basic receive prefill

--- app/controllers/ReceiveController.scala
+++ app/controllers/ReceiveController.scala
@@ -8,10 +8,36 @@ import models._
 
 object ReceiveController extends Controller with Secured {
 
+  val simpleRegisterForm = Form {
+    "title" -> optional(text(maxLength = 100))
+  }
+
+  val fullPresentForm = Form {
+    tuple(
+      "title" -> text(minLength=2, maxLength = 100),
+      "description" -> optional(text(maxLength = 2000)),
+      "from" -> text(minLength=2, maxLength = 100)
+    )
+  }
+
   def showReceive = withSessionPerson { sessionPerson => implicit request =>
 
     Ok(views.html.receive.receive(sessionPerson))
 
   }
+
+  def startPresentRegistration = withSessionPerson { sessionPerson => implicit request =>
+    simpleRegisterForm.bindFromRequest.fold (
+      errors => {
+        Ok(views.html.receive.recordpresent(sessionPerson,fullPresentForm))
+      },
+      presentTitle => {
+
+        Ok(views.html.receive.recordpresent(sessionPerson,fullPresentForm.fill(presentTitle.getOrElse(""),None,"")))
+      }
+    )
+  }
+
+  def registerPresent = TODO
 
 }
