b7a22c60f9806e7dd0d66a4abbb1e59e0b3402c2
ivar <ivar@flurdy.com>
2012-12-20 00:51:27 +0000

    loggin uri

--- app/models/MongoRepository.scala
+++ app/models/MongoRepository.scala
@@ -3,12 +3,16 @@ package models
 import com.mongodb.casbah._
 import play.api.Play
 import play.api.Play.current
+import akka.event.slf4j.Logger
 
 object MongoRepository {
 
   val mongoUri = MongoURI(Play.configuration.getString("mongodb.uri").get)
 
-  def getMongoCollection(collectionName:String) : MongoCollection = mongoUri.connectDB fold ( throw _, _.apply(collectionName) )
+  def getMongoCollection(collectionName:String) : MongoCollection = {
+    Logger.info("uri %s".format(Play.configuration.getString("mongodb.uri").get))
+    mongoUri.connectDB fold ( throw _, _.apply(collectionName) )
+  }
 
 
 }
