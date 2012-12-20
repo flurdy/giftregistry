0afb6ac8c8fdecd4ba9024e0da06d6f13686883a
ivar <ivar@flurdy.com>
2012-12-20 01:29:30 +0000

    loggin uri auth

--- app/models/MongoRepository.scala
+++ app/models/MongoRepository.scala
@@ -9,8 +9,15 @@ object MongoRepository {
   val mongoUri = MongoURI(Play.configuration.getString("mongodb.uri").get)
 
   def getMongoCollection(collectionName:String) : MongoCollection = {
-    Logger.info("uri %s".format(Play.configuration.getString("mongodb.uri").get))
-    mongoUri.connectDB fold ( throw _, _.apply(collectionName) )
+    mongoUri.connectDB match {
+      case Left(thrown) => throw thrown
+      case Right(database) => {
+        Logger.info("Is auth %b".format( database.isAuthenticated ) )
+        database.authenticate(mongoUri.username.get,mongoUri.password.get.toString)
+        Logger.info("Is auth %b".format( database.isAuthenticated ) )
+        database.apply(collectionName)
+      }
+    }
   }
 
 
